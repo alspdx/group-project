958f1ebe44e93a5261370c748b8815155c42814b
Adam Smith and Mikey Pinaud <adam@>
2017-11-06 11:17:21 -0800

    add ability to click pedal name and display pedal information

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-
 // backend
 
 // pedal constructor
-function Pedal(pedalName, type, link, image, info){
+function Pedal(name, brand, type, link, image, info){
   this.name = name;
   this.brand = brand;
   this.type = type;
@@ -20,16 +19,17 @@ function Artist(artistName, group, pedals, info) {
 }
 
 // pedal variables
-var bossDS1 = new Pedal ("Boss DS-1", "Boss", "Distortion", "http://www.effectsdatabase.com/model/boss/compact/ds1", "img/bossDS1.jpg", "This is the classic distortion pedal launched a million guitar solos. Known by professional guitarists the world over as 'that orange BOSS distortion,' the DS-1 Distortion is still in production to this day, thanks to its distinct growling-yet-warm tone and bite. Plug it into your favorite stack and see what we mean.");
+var pedalsArray = [];
 
-var bossRV3= new Pedal ("Boss RV-3", "Boss", "Reverb/Delay", "http://www.effectsdatabase.com/model/boss/compact/rv3#related", "img/bossRV3.jpg", "The RV-3 is a popular professional-quality pedal that can produce reverb, delay or both. It offers four different high-density reverb modes plus three delay modes offering delay times up to 2,000ms.");
+pedalsArray.push(new Pedal ("Boss DS-1", "Boss", "Distortion", "http://www.effectsdatabase.com/model/boss/compact/ds1", "img/bossDS1.jpg", "This is the classic distortion pedal launched a million guitar solos. Known by professional guitarists the world over as 'that orange BOSS distortion,' the DS-1 Distortion is still in production to this day, thanks to its distinct growling-yet-warm tone and bite. Plug it into your favorite stack and see what we mean."));
+
+pedalsArray.push(new Pedal ("Boss RV-3", "Boss", "Reverb/Delay", "http://www.effectsdatabase.com/model/boss/compact/rv3#related", "img/bossRV3.jpg", "The RV-3 is a popular professional-quality pedal that can produce reverb, delay or both. It offers four different high-density reverb modes plus three delay modes offering delay times up to 2,000ms."));
 
 // artist variables
 var kevinParker = new Artist ("Kevin Parker", "Tame Impala", "Boss DS-1", "Kevin Parker is an Australian singer, songwriter, multi-instrumentalist and record producer, best known for being the frontman and live guitarist of the Australian psychedelic rock band Tame Impala, for which he writes, records, and produces the music.");
 
 var kurtVile = new Artist ("Kurt Vile", "Kurt Vile & The Violators", "Boss RV-3", "Kurt Vile is an American singer, songwriter, multi-instrumentalist and record producer. He is known for his solo work and as the former lead guitarist of rock band The War on Drugs. Both in the studio and during live performances, Vile is accompanied by his backing band, The Violators, which currently includes Jesse Trbovich (bass, guitar, saxophone), Rob Laakso (guitar, bass) and Kyle Spence (drums).");
 
-var newArray = ["Boss DS-1", "Electro Harmonix Small Stone", "Strymon Big Sky", "MXR Phase 90", "Diamond Compressor"]
 
 
 // Front end
@@ -44,9 +44,10 @@ $(function() {
   $("#pedal-list").on('click', 'li', function() {
     var pedalName = $(this).text();
     console.log("li tag " + pedalName);
-    newArray.map(function(arrayItem) {
-      if (arrayItem === pedalName);
-      $("#pedal-info-output").html("<li>" + arrayItem + "</li>");
+    pedalsArray.map(function(pedal) {
+      if (pedal.name === pedalName) {
+        $("#pedal-info-output").html("<li>" + pedal.info + "</li>");
+      };
     });
   });
 });
